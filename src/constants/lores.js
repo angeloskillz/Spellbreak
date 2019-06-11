e2d4aa25b50d5d58b4547b65991c932c89de79c1
Angelo Cantone <angelocantone@imac-di-angelo.homenet.telecomitalia.it>
2019-06-11 21:00:11 +0200

    Lores fix

--- src/constants/lores.js
+++ src/constants/lores.js
@@ -1,8 +1,13 @@
 export default [
     {
+        title: "Homecoming - Release 26 Prologue",
+        link: `/lore//2019-06-11-homecoming-release-26-prologue/`,
+        description: ["Book 1 - Page 49,38,44 & Book 4 - Page 53"],
+    },
+    {
         title: "Whispers of the Vault - Release 25 Prologue",
-        link: `/lore/2019-05-30-scouting-the-storm-release-19-prologue/`,
-        description: ["Book 2 - Page 489"],
+        link: `/lore/2019-05-31-whispers-of-the-vault-release-25-prologue/`,
+        description: ["Book 2 - Page 489 & Book 3 - Page 12"],
     },
     {
         title: "Mystery of the Runes: Release 24 Prologue",
@@ -16,12 +21,12 @@ export default [
     },
     {
         title: "The Fracture: Release 22 Prologue",
-        link: `/lore/2019-05-30-scouting-the-storm-release-19-prologue/`,
+        link: `/lore/2019-05-31-the-fracture-release-22-prologue/`,
         description: ["Book 4 - Page 38,42"],
     },
     {
         title: "Path of the Breaker: Release 21 Prologue",
-        link: `/lore/2019-05-31-the-fracture-release-22-prologue/`,
+        link: `/lore/2019-05-31-path-of-the-breaker-release-21-prologue/`,
         description: ["Book 4 - Page 91"],
     },
     {
